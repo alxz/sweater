2e61d19091888df80713864b25f21fbed7b60083
alxz <azap_ad@yahoo.com>
2023-11-25 17:13:02 -0500

    config changes

---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,17 @@
+package com.example.sweater.domain;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity  // This tells Hibernate to make a table out of this class
+public class Message {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String text;
+    private String tag;
+
+}
