4fff61a9fe9bc0479ae4209f2793deaf5323d3d4
alxz <azap_ad@yahoo.com>
2023-11-25 21:44:19 -0500

    Added files for Spring JPA and code modified. Lessaon 2 completed

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -1,8 +1,10 @@
 package com.example.sweater.domain;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+//Instead of <javax.persistence> for Java17 use: <jakarta.persistence>
+
 
 
 @Entity  // This tells Hibernate to make a table out of this class
@@ -14,4 +16,35 @@ public class Message {
     private String text;
     private String tag;
 
+    public Message() {
+    }
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
