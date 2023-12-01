1dca9610e80d162cf0ff6838582619695657527f
alxz <azap_ad@yahoo.com>
2023-11-30 20:08:32 -0500

    Addeding Spring Security - in progress...

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -15,21 +15,21 @@ public class GreetingController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World")
-                String name, Map <String, Object> model) {
-                    model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map <String, Object> model) {
+           // @RequestParam(name="name", required=false, defaultValue="World")
+
+                    //model.put("name", name);
                     return "greeting";
     }
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map <String,Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages",messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/")
     public String add(@RequestParam String text, @RequestParam String tag, Map <String,Object> model) {
         Message message = new Message(text, tag);
 
